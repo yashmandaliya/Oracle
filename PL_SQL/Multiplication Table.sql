cdd234c6fe577a62101e58bc668e0a08febdf33c
Yash Mandaliya <112624754+yashmandaliya@users.noreply.github.com>
2022-09-19 12:09:27 +0530

    Add Files Via Upload

--- PL_SQL/Multiplication Table.sql
+++ PL_SQL/Multiplication Table.sql
@@ -7,3 +7,22 @@ LOOP
     END LOOP;
 END;
 /
+
+/* 
+Output:-
+
+Enter value for n: 5
+old   2: N NUMBER := &N;
+new   2: N NUMBER := 5;
+5*1=5
+5*2=10
+5*3=15
+5*4=20
+5*5=25
+5*6=30
+5*7=35
+5*8=40
+5*9=45
+5*10=50
+
+*/
